7285a2021a1853d200b046da62c5428d684f9834
daniel <dramirezn18@gmail.com>
2023-11-14 13:46:36 -0500

    variables globales

---
+++ src/scope/global.js
@@ -0,0 +1,23 @@
+// variables
+var a; // declarar
+a = "a"; // asignar
+var b = "b"; // declarar / asignar
+var a = "aa"; // redeclarar
+b = "bb"; // deasignar
+
+var fruit = "Banana";
+
+function printFruit() {
+    console.log(fruit);
+}
+
+printFruit();
+
+nombre = "Mario";
+
+function printName() {
+    nombre = "Pedro";
+}
+
+printName();
+console.log(nombre);
